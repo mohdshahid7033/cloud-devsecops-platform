b4b1ef888acb6838977b215ed4061abec9d093b8
Mohd Shahid <shahidmohd7033@gmail.com>
2026-09-21 18:11:09 +0000

    Add SonarQube CI and Nginx reverse proxy

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,14 +68,6 @@ resource "aws_security_group" "devsecops_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "DevSecOps application"
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
