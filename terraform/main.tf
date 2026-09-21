015e59ec2c632e15e10638b89f7a8fac62c1afee
Mohd Shahid <shahidmohd7033@gmail.com>
2026-09-21 10:01:19 +0000

    Harden EC2 security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,13 +75,7 @@ resource "aws_security_group" "devsecops_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
   egress {
     from_port   = 0
     to_port     = 0
