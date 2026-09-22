fa75dfdf648e6f1bba788b715fdd51344c62a74c
Mohd Shahid <shahidmohd7033@gmail.com>
2026-09-22 17:30:24 +0000

    Automate monitoring deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,6 +68,14 @@ resource "aws_security_group" "devsecops_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
