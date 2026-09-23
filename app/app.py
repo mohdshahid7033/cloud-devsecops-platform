fd73f6479ad0749c64747c3efd13d29fdcbf121c
Mohd Shahid <shahidmohd7033@gmail.com>
2026-09-23 16:07:44 +0530

    Verify automated CI/CD deployment

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ metrics = PrometheusMetrics(app)
 
 @app.route("/", methods=["GET"])
 def home():
-    return "Cloud-Based DevSecOps Platform is running!"
+    return "Cloud-Based DevSecOps Platform is running! CI/CD deployment verified."
 
 
 @app.route("/health", methods=["GET"])
